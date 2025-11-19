a7d37755e653f3426652dee4f0f223b4a7a4380c
claudiopb <hai_madre@hotmail.com>
2025-11-19 09:12:57 -0600

    summary

--- practica1/alumnos/views.py
+++ practica1/alumnos/views.py
@@ -6,5 +6,5 @@ from django.http import HttpResponse
 def inicio(request):
     return render(request,'myfirst.html')
 
-def nosostros(request):
+def nosotros(request):
     return render(request,'nosotros.html')
