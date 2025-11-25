a8dd50943795615bd9c83b796e2d8f935efd511c
claudiopb <hai_madre@hotmail.com>
2025-11-25 09:34:51 -0600

    Summary

--- practica1/alumnos/urls.py
+++ practica1/alumnos/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    path('alumnos', views.alumnos, name='alumnos'),
     path('nosotros', views.nosotros, name='nosotros'),
+    path('crear', views.crear, name='crear'),
+    path('editar', views.editar, name='editar'),
 ]
--- practica1/alumnos/views.py
+++ practica1/alumnos/views.py
@@ -6,5 +6,14 @@ from django.http import HttpResponse
 def inicio(request):
     return render(request,'myfirst.html')
 
+def alumnos(request):
+    return render(request,'lista_alumnos.html')
+
 def nosotros(request):
     return render(request,'nosotros.html')
+
+def crear(request):
+    return render(request,'crear.html')
+
+def editar(request):
+    return render(request,'editar.html')
